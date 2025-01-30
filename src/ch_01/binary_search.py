32cf411116fc8dda37ac7b1ea65eb3c41bbb4e68
Danial Kurtumerov <kur7umerovd@gmail.com>
2025-01-30 11:14:37 +0500

    feat: binary search exercises
    Added exercises for binary search algorithm
    from "Chapter 1. Introduction to algorithms".
    Which led to implement lower bound binary search
    algorithm.

--- src/ch_01/binary_search.py
+++ src/ch_01/binary_search.py
@@ -93,13 +93,50 @@ def binary_search(
     return None
 
 
+def lower_bound_binary_search(
+    sequence: Sequence[Comparable],
+    element: Comparable,
+) -> int | None:
+    """Perform lower bound binary search of element on a sequence.
+
+    Args:
+        sequence (Sequence[Comparable]): Any sequence compatible type.
+        element (Comparable): Any element
+
+    Returns:
+        int | None: index of an element in sequence. None if element not in
+            sequence
+
+    """
+    left_border: int = 0
+    right_border: int = len(sequence) - 1
+
+    while left_border < right_border:
+        cursor = (left_border + right_border) // 2
+        guess: Comparable = sequence[cursor]
+
+        if guess < element:
+            left_border = cursor + 1
+
+        else:
+            right_border = cursor
+
+    if (
+        left_border < len(sequence)
+        and sequence[left_border] == element
+    ):
+        return left_border
+
+    return None
+
+
 def binary_search_debug(
     sequence: Sequence[Comparable],
     element: Comparable,
     *,
     verbose: bool = False,
 ) -> BinarySearchDebug:
-    """Perform binary search of element on a sequence, with debug information.
+    """Perform binary search of element on a sequence, with debug.
 
     Please note that debug version is slower than original.
 
@@ -134,6 +171,55 @@ def binary_search_debug(
             log(str(debug.as_dict()), verbose=verbose)
 
             return debug
+
+    debug.stop_timer()
+    log(str(debug.as_dict()), verbose=verbose)
+    return debug
+
+
+def lower_bound_binary_search_debug(
+    sequence: Sequence[Comparable],
+    element: Comparable,
+    *,
+    verbose: bool = False,
+) -> BinarySearchDebug:
+    """Perform lower bound binary search of element on a sequence, with debug.
+
+    Please note that debug version is slower than original.
+
+    Args:
+        sequence (Sequence[Comparable]): Any sequence compatible type.
+        element (Comparable): Any element
+        verbose (bool): Print debug to console at every step.
+
+    Returns:
+        BinarySearchDebug: debug structure for binary search algorithm.
+
+    """
+    debug: BinarySearchDebug = BinarySearchDebug()
+    debug.start_timer()
+    debug.right_border = len(sequence) - 1
+
+    while debug.left_border < debug.right_border:
+        debug.iterations += 1
+        debug.cursor = (debug.left_border + debug.right_border) // 2
+        guess: Comparable = sequence[debug.cursor]
+        log(str(debug.as_dict()), verbose=verbose)
+
+        if guess < element:
+            debug.left_border = debug.cursor + 1
+
+        else:
+            debug.right_border = debug.cursor
+
+    if (
+        debug.left_border < len(sequence)
+        and sequence[debug.left_border] == element
+    ):
+        debug.index = debug.left_border
+        debug.stop_timer()
+        log(str(debug.as_dict()), verbose=verbose)
+        return debug
 
     debug.stop_timer()
     log(str(debug.as_dict()), verbose=verbose)
--- tests/ch_01/test_binary_search.py
+++ tests/ch_01/test_binary_search.py
@@ -1,6 +1,13 @@
 """Tests for Chapter 1. Introduction to algorithms. Binary search."""
 
-from src.ch_01.binary_search import binary_search, binary_search_debug
+from math import log2
+
+from src.ch_01.binary_search import (
+    binary_search,
+    binary_search_debug,
+    lower_bound_binary_search,
+    lower_bound_binary_search_debug,
+)
 
 
 def test_binary_search_on_integers() -> None:
@@ -10,8 +17,14 @@ def test_binary_search_on_integers() -> None:
     assert binary_search(sequence, 3) == 1
     assert binary_search(sequence, -1) is None
 
+    assert lower_bound_binary_search(sequence, 3) == 1
+    assert lower_bound_binary_search(sequence, -1) is None
+
     assert binary_search_debug(sequence, 3).index == 1
     assert binary_search_debug(sequence, -1).index is None
+
+    assert lower_bound_binary_search_debug(sequence, 3).index == 1
+    assert lower_bound_binary_search_debug(sequence, -1).index is None
 
 
 def test_binary_search_on_floats() -> None:
@@ -21,8 +34,14 @@ def test_binary_search_on_floats() -> None:
     assert binary_search(sequence, 2.55) == 2
     assert binary_search(sequence, -5) is None
 
+    assert lower_bound_binary_search(sequence, 2.55) == 2
+    assert lower_bound_binary_search(sequence, -5) is None
+
     assert binary_search_debug(sequence, 2.55).index == 2
     assert binary_search_debug(sequence, -5).index is None
+
+    assert lower_bound_binary_search_debug(sequence, 2.55).index == 2
+    assert lower_bound_binary_search_debug(sequence, -5).index is None
 
 
 def test_binary_search_on_strings() -> None:
@@ -32,5 +51,80 @@ def test_binary_search_on_strings() -> None:
     assert binary_search(sequence, "a") == 0
     assert binary_search(sequence, "d") == 3
 
+    assert lower_bound_binary_search(sequence, "a") == 0
+    assert lower_bound_binary_search(sequence, "d") == 3
+
     assert binary_search_debug(sequence, "a").index == 0
     assert binary_search_debug(sequence, "d").index == 3
+
+    assert lower_bound_binary_search_debug(sequence, "a").index == 0
+    assert lower_bound_binary_search_debug(sequence, "d").index == 3
+
+
+def test_exercise_1_1() -> None:
+    """Test exercise 1.1."""
+    amount: int = 128
+    sequence: list[int] = list(range(1, amount + 1))
+    expected_iterations: int = int(log2(amount))
+
+    assert len(sequence) == amount
+
+    assert sequence[0] == 1
+    assert sequence[-1] == amount
+
+    # First element will need log2(n) steps.
+    assert binary_search_debug(
+        sequence, sequence[0],
+    ).iterations == expected_iterations
+
+    # Last element will need log2(n) + 1 steps.
+    assert binary_search_debug(
+        sequence, sequence[-1],
+    ).iterations == expected_iterations + 1
+
+    # But "lower bound binary search" will need log2(n) for that.
+    assert lower_bound_binary_search_debug(
+        sequence, sequence[-1],
+    ).iterations == expected_iterations
+
+    # Element that not in a sequence will need log2(n) + 1 steps.
+    assert binary_search_debug(
+        sequence, sequence[-1] + 1,
+    ).iterations == expected_iterations + 1
+
+    # But "lower bound binary search" will need log2(n) for that.
+    assert lower_bound_binary_search_debug(
+        sequence, sequence[-1] + 1,
+    ).iterations == expected_iterations
+
+
+def test_exercise_1_2() -> None:
+    """Test exercise 1.2."""
+    amount: int = 128 * 2
+    sequence: list[int] = list(range(1, amount + 1))
+    expected_iterations: int = int(log2(amount))
+
+    assert len(sequence) == amount
+
+    assert sequence[0] == 1
+    assert sequence[-1] == amount
+
+    assert binary_search_debug(
+        sequence, sequence[0],
+    ).iterations == expected_iterations
+
+    assert binary_search_debug(
+        sequence, sequence[-1],
+    ).iterations == expected_iterations + 1
+
+    assert lower_bound_binary_search_debug(
+        sequence, sequence[-1],
+    ).iterations == expected_iterations
+
+    assert binary_search_debug(
+        sequence, sequence[-1] + 1,
+    ).iterations == expected_iterations + 1
+
+    assert lower_bound_binary_search_debug(
+        sequence, sequence[-1] + 1,
+    ).iterations == expected_iterations
